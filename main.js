9b420d16e4dbbf1ac3fad6e52fda4eb4c28cce20
Svyat <43937124+Svyatyk@users.noreply.github.com>
2021-11-17 11:33:58 +0200

    Minor fixes in skills bars

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
     const BAR = document.querySelectorAll('.progressbar');
-
+    
     BAR.forEach(function(bar){
         let percentage = bar.dataset.percent;
-        let scale = bar.clildren[0];
+        let scale = bar.childNodes;
         scale.innerText = percentage + '%';
         bar.style.width = percentage + '%';
         console.log(percentage);
     });
-})
-//TODO fix
+
+
 
 
 
